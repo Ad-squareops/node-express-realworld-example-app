9dc8c6e0f9b0aeb5d95c13c867682afc37406301
Ad-squareops <118443159+Ad-squareops@users.noreply.github.com>
2023-02-16 10:34:37 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,3 +2,7 @@
 echo "Update Server before Work"
 apt update 
 echo "Server Updated"
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:AmazonCloudWatch-linux-adi-L2P6 -s
+sudo systemctl start amazon-cloudwatch-agent.service
+sudo systemctl enable amazon-cloudwatch-agent.service
+sudo systemctl status amazon-cloudwatch-agent.service
